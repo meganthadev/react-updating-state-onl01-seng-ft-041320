01b5997b7880a01ab673d2479ff0fde555b4c922
Megan Horsfield <meganthadev@gmail.com>
2020-08-21 07:42:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
-import ClickityClick from './components/ClickityClick';
- 
-ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+
+ReactDOM.render(
+  <div>
+    Mount Components Here
+  </div>,
+  document.getElementById('root')
+);
